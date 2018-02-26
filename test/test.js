6002c47be9b4f47b9cf6562088e8f2e3f1277420
kgjin.dev <kgjin.dev@gmail.com>
2018-02-26 21:55:37 +0900

    api updated

--- test/test.js
+++ test/test.js
@@ -1,18 +1,20 @@
-var assert = require('assert');
-var cpClient = require('..');
+const assert = require('assert');
+const Couponpapa = require('..');
 
 describe('API', function() {
-  const client = new cpClient('YOUR-KEY', 'http://localhost:7701');
+  const cpp = new Couponpapa('YOUR-API-KEY', 'sandbox');
   const CODE = 'TESTCODE';
   const trackingId = 'test@couponpapa.io';
+  const orderId = 1;
+  const tag = 'NEWUSER';
 
   describe('check', function() {
     it('code: ' + CODE, async () => {
       try {
-        const result = await client.check(CODE, trackingId, 100);
+        const result = await cpp.codesCheck(CODE, trackingId, 100);
         assert.equal(result.code, CODE);
       } catch (e) {
-        assert.equal(e, 'wrong code');
+        assert.equal(e, 'WRONG CODE');
       }
     });
   });
@@ -20,17 +22,42 @@ describe('API', function() {
   describe('use', function() {
     it('code: ' + CODE, async () => {
       try {
-        const result = await client.use(CODE, trackingId, 100);
+        const result = await cpp.codesUse(CODE, trackingId, 100, orderId);
         assert.equal(result.code, CODE);
       } catch (e) {
-        assert.equal(e, 'wrong code');
+        assert.equal(e, 'WRONG CODE');
       }
+    });
+  });
+
+  describe('cancel', function() {
+    it('cancel order ID: ' + orderId, async () => {
+      try {
+        const result = await cpp.codesCancel(orderId);
+        assert.equal(result.code, CODE);
+      } catch (e) {
+        assert.equal(e, 'OrderId not found - No code to cancel');
+      }
+    });
+  });
+
+  describe('fire', function() {
+    it('tag: ' + tag, async () => {
+      const result = await cpp.codesFire(tag, trackingId);
+      assert.equal(result.sent, 0);
+    });
+  });
+
+  describe('list', function() {
+    it('trackingId: ' + trackingId, async () => {
+      const result = await cpp.couponboxesList(trackingId, 100);
+      assert.equal(result.coupons.length, 0);
     });
   });
 
   describe('repurchase', function() {
     it('trackingId: ' + trackingId, async () => {
-      const result = await client.repurchase(trackingId, 10);
+      const result = await cpp.analyticsRepurchase(trackingId, 10);
       assert.equal(result.trackingId, trackingId);
     });
   });
